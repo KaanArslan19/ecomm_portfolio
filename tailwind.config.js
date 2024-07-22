bd7e5a1e86f0fadf5451e8cffb57f7d93497a34e
KaanArslan19 <kaanarslan35@gmail.com>
2024-07-22 18:31:15 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#242424",
+        primaryLight: "#24242431",
+        secondary: "#d0fb49",
+      },
+    },
+  },
+  plugins: [],
+});
