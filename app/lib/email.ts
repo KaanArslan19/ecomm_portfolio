83fdbd37c1d02bbe14649335cc81d3b7b197751a
KaanArslan19 <kaanarslan35@gmail.com>
2024-08-19 15:58:17 +0300

    send email method was updated for real usage

--- app/lib/email.ts
+++ app/lib/email.ts
@@ -1,7 +1,22 @@
 import { profile } from "console";
 import nodemailer from "nodemailer";
+import { MailtrapClient } from "mailtrap";
 
 type profile = { name: string; email: string };
+
+interface CustomMailtrapClientConfig {
+  token: string;
+  endpoint?: string;
+}
+const client = new MailtrapClient({
+  endpoint: process.env.MAILTRAP_ENDPOINT,
+  token: process.env.MAILTRAP_TOKEN,
+} as CustomMailtrapClientConfig);
+
+const sender = {
+  email: "verification@nextecommportfoliokaanarslan.xyz",
+  name: "Next E-comm Verification",
+};
 
 interface EmailOptions {
   profile: profile;
@@ -21,29 +36,68 @@ const generateMailTransporter = () => {
   return transport;
 };
 const sendEmailVerificationLink = async (profile: profile, linkUrl: string) => {
-  const transport = generateMailTransporter();
+  /*   const transport = generateMailTransporter();
   await transport.sendMail({
     from: "verification@nextecom.com",
     to: profile.email,
     html: `<h1> Please verify your email by clicking on <a href=${linkUrl}>this link</a> </h1>`,
+  }); */
+  const recipients = [
+    {
+      email: profile.email,
+    },
+  ];
+
+  await client.send({
+    from: sender,
+    to: recipients,
+    subject: "Verify your email!",
+    text: `<h1> Please verify your email by clicking on <a href=${linkUrl}>this link</a> </h1>`,
+    category: "Email Verification",
   });
 };
 const sendForgetPasswordLink = async (profile: profile, linkUrl: string) => {
-  const transport = generateMailTransporter();
+  /*   const transport = generateMailTransporter();
 
   await transport.sendMail({
     from: "verification@nextecom.com",
     to: profile.email,
     html: `<h1>Please verify your email by clicking on <a href=${linkUrl}>this link</a> </h1>`,
+  }); */
+  const recipients = [
+    {
+      email: profile.email,
+    },
+  ];
+
+  await client.send({
+    from: sender,
+    to: recipients,
+    subject: "Forget Password Link",
+    text: `<h1> Click on <a href=${linkUrl}>this link</a> to reset your password </h1>`,
+    category: "Forget Password Link",
   });
 };
 const sendUpdatePasswordConfirmation = async (profile: profile) => {
-  const transport = generateMailTransporter();
+  /*   const transport = generateMailTransporter();
 
   await transport.sendMail({
     from: "verification@nextecom.com",
     to: profile.email,
     html: `<h1>Your password is now changed <a href=${process.env.SIGN_IN_URL}>click here </a>to sign in</h1>`,
+  }); */
+  const recipients = [
+    {
+      email: profile.email,
+    },
+  ];
+
+  await client.send({
+    from: sender,
+    to: recipients,
+    subject: "Password Reset",
+    text: `<h1>Your password is now changed <a href=${process.env.SIGN_IN_URL}>click here </a>to sign in</h1>`,
+    category: "Password Reset",
   });
 };
 export const sendEmail = (options: EmailOptions) => {
